f7d53bf50547a8096f9309424d1cf735f6bd27c2
Yoel Leghesse <114878101+yoelleghesse@users.noreply.github.com>
2024-03-09 17:49:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def get_news(country, api_key='3a5434d4782c4d7dbef028394a92ef41'):
+  url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}'
+  r = requests.get(url)
+  content = r.json()
+  articles = content['articles']
+  results = []
+  for article in articles:
+    results.append(f"TITLE\n'{article['title']}, '\nDESCRIPTION\n', {article['description']}")
+  return results
+
+print(get_news(country='us'))
+
+
+
